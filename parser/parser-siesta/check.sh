52786f6066ac6a56598d564f4616c3dd01661655
Ask Hjorth Larsen <asklarsen@gmail.com>
2016-09-15 21:21:17 +0200

    improve check file

--- parser/parser-siesta/check.sh
+++ parser/parser-siesta/check.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-python main.py test/H2O/out --annotate
-python main.py test/H2O-relax/out --annotate
-python main.py test/Al-bulk/out --annotate
-python main.py test/Al-slab/out --annotate
-python main.py test/MgO/out --annotate
-python main.py test/smeagol-Au-leads/Au.out --annotate
-python main.py test/smeagol-Au-scregion/Au.out --annotate
+python main.py test/H2O/out --annotate > test/H2O/json 2>&1
+python main.py test/H2O-relax/out --annotate > test/H2O-relax/json 2>&1
+python main.py test/Al-bulk/out --annotate > test/Al-bulk/json 2>&1
+python main.py test/Al-slab/out --annotate > test/Al-slab/json 2>&1
+python main.py test/MgO/out --annotate > test/MgO/json 2>&1
+python main.py test/smeagol-Au-leads/Au.out --annotate > test/smeagol-Au-leads/json 2>&1
+python main.py test/smeagol-Au-scregion/Au.out --annotate > test/smeagol-Au-scregion/json 2>&1

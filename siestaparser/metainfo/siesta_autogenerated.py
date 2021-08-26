01a51e167063937b81bc9f8929777366c73cf38a
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2021-08-27 01:17:59 +0200

    Adapted further metainfo changes

--- siestaparser/metainfo/siesta_autogenerated.py
+++ siestaparser/metainfo/siesta_autogenerated.py
@@ -25,6 +25,9 @@ from nomad.metainfo import (  # pylint: disable=unused-import
 from nomad.datamodel.metainfo import run
 
 
+m_package = Package()
+
+
 class x_siesta_input(MCategory):
     '''
     siesta input variables

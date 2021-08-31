2c055bd71c6b12e49d4afa856640e040bb36b3a4
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2021-08-31 14:33:02 +0200

    Adapted further changes in metainfo

--- siestaparser/metainfo/siesta_autogenerated.py
+++ siestaparser/metainfo/siesta_autogenerated.py
@@ -22,7 +22,7 @@ from nomad.metainfo import (  # pylint: disable=unused-import
     MSection, MCategory, Category, Package, Quantity, Section, SubSection, SectionProxy,
     Reference
 )
-from nomad.datamodel.metainfo import run
+from nomad.datamodel.metainfo import simulation
 
 
 m_package = Package()
@@ -2980,7 +2980,7 @@ class x_siesta_section_input(MSection):
         categories=[x_siesta_input])
 
 
-class Method(run.method.Method):
+class Method(simulation.method.Method):
 
     m_def = Section(validate=False, extends_base_section=True)
 
